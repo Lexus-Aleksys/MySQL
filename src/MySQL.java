0512c30f16bb7dda07ffa09a4653ac9da0313d06
Шамрило Алексей Сергеевич <shamrilo.as@att.edu.ru>
2023-09-08 10:02:35 +0300

    Initial commit

---
+++ src/MySQL.java
@@ -0,0 +1,26 @@
+public class MySQL {
+
+    public String parsLine (String line){
+        return "" ;
+    }
+    private String insert (String liine){
+        return "";
+    }
+    private String update (String liine){
+        return "";
+    }
+    private String delete (String liine){
+        return "";
+    }
+    private String select (String liine){
+        return "";
+    }
+    public class Student {
+        int id;
+        String lastName;
+        int age;
+        double cost;
+        boolean active;
+    }
+
+}
